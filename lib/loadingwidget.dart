de206b08acb708f70031717ee9ef804717af3f43
gregorysaml <gregorysam1997@hotmail.gr>
2021-11-17 19:36:37 +0200

    initial commit

---
+++ lib/loadingwidget.dart
@@ -0,0 +1,135 @@
+// ignore_for_file: avoid_print, file_names
+
+import 'package:flutter/material.dart';
+import 'package:graphql_bug/succ.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+class LoadingPage extends StatefulWidget {
+  final String userId;
+  final String userPhone;
+
+  const LoadingPage({Key key, this.userId, this.userPhone}) : super(key: key);
+
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+  Map userdata;
+  Map mutation;
+  bool isqueryed = false;
+  bool existinUser = false;
+
+  Future<Map> getQuery({userId}) async {
+    GraphQLClient client = GraphQLProvider.of(context).value;
+    var userdata = await client.query(QueryOptions(
+      fetchPolicy: FetchPolicy.networkOnly,
+      document: gql(getUserData()),
+    ));
+    // ignore: unrelated_type_equality_checks
+    if (userdata.exception == true) {
+      print(userdata.exception);
+      // throw (userdata.exception);
+    }
+    if (userdata.isLoading) {
+      print('userdata.isLoading: $userdata.isLoading');
+    }
+    print('userdata1: $userdata');
+
+    isqueryed = true;
+    if (userdata.data.isNotEmpty) {
+      existinUser = true;
+    }
+
+    return userdata.data;
+  }
+
+  String createUser() {
+    return '''mutation (\$input:String!) {
+  createPost(input: \$input) {
+    id
+    title
+    body
+  }
+} ''';
+  }
+
+  String getUserData() {
+    return '''query {
+  user(id: 1) {
+    id
+    username
+    email
+    address {
+      geo {
+        lat
+        lng
+      }
+    }
+  }
+}
+    ''';
+  }
+
+  Future nav(BuildContext context) async {
+    WidgetsFlutterBinding.ensureInitialized();
+
+    Future.delayed(Duration.zero).then((value) => {
+          // Navigator.push(
+          //     context, MaterialPageRoute(builder: (context) => const Name()))
+          //with push named works
+          Navigator.of(context).pushNamed(
+            '/trea',
+          )
+        });
+  }
+
+  @override
+  void initState() {
+    //  getQuery();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // final args =
+    // ModalRoute.of(context).settings.arguments as Map<String, dynamic>;
+
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Mutation(
+              options: MutationOptions(
+                  cacheRereadPolicy: CacheRereadPolicy.ignoreAll,
+                  onCompleted: (data) {},
+                  onError: (data) {},
+                  document: gql(createUser())),
+              builder: (RunMutation runMutation, QueryResult result) {
+                getQuery();
+
+                runMutation({
+                  'input': {
+                    "title": "A Very Captivating Post Title",
+                    "body": "Some interesting content."
+                  }
+                });
+                userdata = result.data;
+
+                WidgetsBinding.instance.addPostFrameCallback((_) {
+                  nav(context);
+                });
+
+                return const SizedBox.shrink();
+              }),
+        ],
+      ),
+    ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:graphql_bug/loadingWidget.dart';
+import 'package:graphql_bug/succ.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+Future<void> main() async {
+  await initHiveForFlutter();
+  final HttpLink _httpLink = HttpLink('https://graphqlzero.almansi.me/api');
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(cache: GraphQLCache(store: HiveStore()), link: _httpLink),
+  );
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(GraphQLProvider(client: client, child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyHomePage(),
+        '/second': (context) => const LoadingPage(),
+        '/trea': (context) => const Name(),
+      },
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoadingPage()));
+                },
+                child: const Text('Create a user'))
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
